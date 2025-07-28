b05f44ccb596be3ec22dfd629b9fa0e820f6387a
Dominictech892 <johndominic892@gmail.com>
2025-07-28 11:56:23 +0300

    updates

---
+++ app/src/main/java/com/marco/bizmall/navigation/AppNavHost.kt
@@ -0,0 +1,60 @@
+package com.marco.bizmall.navigation
+
+import android.window.SplashScreen
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.marco.bizmall.ui.screens.about.AboutScreen
+import com.marco.bizmall.ui.screens.category.CategoryScreen
+import com.marco.bizmall.ui.screens.contact.ContactScreen
+import com.marco.bizmall.ui.screens.home.HomeScreen
+import com.marco.bizmall.ui.screens.item.ItemScreen
+import com.marco.bizmall.ui.screens.splash.SplashScreen
+
+@Composable
+fun AppNavHost(
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController(),
+    startDestination: String = ROUT_SPLASH
+
+) {
+
+    NavHost(
+        navController = navController,
+        startDestination = startDestination,
+        modifier = modifier
+    ) {
+        composable(ROUT_HOME) {
+            HomeScreen(navController)
+        }
+        composable(ROUT_ABOUT) {
+            AboutScreen(navController)
+        }
+        composable(ROUT_CONTACT) {
+            ContactScreen(navController)
+        }
+        composable(ROUT_ITEM) {
+            ItemScreen(navController)
+        }
+        composable(ROUT_CATEGORY) {
+            CategoryScreen(navController)
+        }
+        composable(ROUT_SPLASH) {
+            SplashScreen(navController)
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
